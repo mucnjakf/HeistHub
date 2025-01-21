8d0dd8978f72013569d5302f61231f41a0691cea
mucnjakf <filip.mucnjak@outlook.com>
2025-01-21 01:44:06 +0100

    Added Application project

---
+++ src/HeistHub.Application/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HeistHub.Application;
+
+public static class Bootstrapper
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        Assembly assembly = typeof(Bootstrapper).Assembly;
+
+        services.AddMediatR(configuration => { configuration.RegisterServicesFromAssembly(assembly); });
+
+        return services;
+    }
+}
