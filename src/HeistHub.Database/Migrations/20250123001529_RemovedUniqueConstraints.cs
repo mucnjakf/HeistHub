2b2ba3f716774017d4003e655f4ee4447ef972e2
mucnjakf <filip.mucnjak@outlook.com>
2025-01-23 02:23:04 +0100

    Minor refactor

---
+++ src/HeistHub.Database/Migrations/20250123001529_RemovedUniqueConstraints.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeistHub.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedUniqueConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Skills_Name",
+                table: "Skills");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Members_Email",
+                table: "Members");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Heists_Name",
+                table: "Heists");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_Name",
+                table: "Skills",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_Email",
+                table: "Members",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Heists_Name",
+                table: "Heists",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
